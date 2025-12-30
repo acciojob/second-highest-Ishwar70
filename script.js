b946ce04ae6570afa7a03bd9dc2174485ec55639
Ishwar <ishwar.cse25@gmail.com>
2025-12-30 17:17:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 <script>
 	function secondHighest(arr) {
-		// convert string inputs to numbers
+		// convert to numbers
 		arr = arr.map(Number);
 
-		// if less than 2 elements
+		// Edge case: less than 2 elements
 		if (arr.length < 2) return -Infinity;
 
-		let highest = -Infinity;
+		let max = -Infinity;
 		let second = -Infinity;
 
-		// find highest and second highest without sorting
 		for (let num of arr) {
-			if (num > highest) {
-				second = highest;
-				highest = num;
-			} else if (num < highest && num > second) {
+			if (num > max) {
+				second = max;
+				max = num;
+			} else if (num < max && num > second) {
 				second = num;
 			}
 		}
 
-		// if second highest was never updated
+		// If no second highest exists (all equal)
+		if (second === -Infinity) return -Infinity;
+
 		return second;
 	}
 
 	function Main() {
-		var n = prompt("Enter the number of elements");
+		var n = parseInt(prompt("Enter the number of elements"));
 		var arr = [];
+
 		for (var i = 0; i < n; i++) {
 			arr[i] = prompt("Enter element " + (i+1));
 		}
-		alert(secondHighest(arr));
+
+		// Cypress compares number => force numeric
+		alert(Number(secondHighest(arr)));
 	}
 	Main();
 </script>
